46d7d4bff36a2ff9f71a6261086e938a60832f03
Tom MTT <tom@heimdall.pm>
2022-11-29 10:30:57 +0100

    style(server.c): macros indentation

--- server.c
+++ server.c
@@ -105,16 +105,17 @@ int initialize_server()
 
         /* Enable dual-stack mode on supported platforms */
         #ifndef __OpenBSD__
-            verbose(3, "  IPV6_V6ONLY...");
-            if (setsockopt(server, IPPROTO_IPV6, IPV6_V6ONLY, &ipv6_only, sizeof(ipv6_only)) < 0)
-                return -1;
+        verbose(3, "  IPV6_V6ONLY...");
+        if (setsockopt(server, IPPROTO_IPV6, IPV6_V6ONLY, &ipv6_only, sizeof(ipv6_only)) < 0)
+            return -1;
+
         #else
-            if (ipv6_only == 0) {
-                puts("");
-                syslog(LOG_WARNING, "dual-stack mode is disabled on OpenBSD.");
-                syslog(LOG_WARNING, "feuille will only listen on the `::' IPv6 address.");
-                puts("");
-            }
+        if (ipv6_only == 0) {
+            puts("");
+            syslog(LOG_WARNING, "dual-stack mode is disabled on OpenBSD.");
+            syslog(LOG_WARNING, "feuille will only listen on the `::' IPv6 address.");
+            puts("");
+        }
         #endif
 
         /* bind address and port */
